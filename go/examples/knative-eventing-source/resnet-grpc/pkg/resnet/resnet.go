39cbae6cd70b11341e2fd6e6067d035770e9eb1a
Huamin Chen <hchen@redhat.com>
2018-11-29 14:13:44 +0000

    demo: add resnet
    Signed-off-by: Huamin Chen <hchen@redhat.com>

---
+++ go/examples/knative-eventing-source/resnet-grpc/pkg/resnet/resnet.go
@@ -0,0 +1,60 @@
+package resnet
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"log"
+
+	tf_core_framework "github.com/tensorflow/tensorflow/tensorflow/go/core/framework"
+	pb "tensorflow_serving/apis"
+
+	"google.golang.org/grpc"
+)
+
+func Predict(servingAddress string, reader io.ReadCloser) *tf_core_framework.TensorProto {
+	imageBytes, err := ioutil.ReadAll(reader)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request := &pb.PredictRequest{
+		ModelSpec: &pb.ModelSpec{
+			Name:          "resnet",
+			SignatureName: "serving_default",
+		},
+		Inputs: map[string]*tf_core_framework.TensorProto{
+			"image_bytes": &tf_core_framework.TensorProto{
+				Dtype: tf_core_framework.DataType_DT_STRING,
+				TensorShape: &tf_core_framework.TensorShapeProto{
+					Dim: []*tf_core_framework.TensorShapeProto_Dim{
+						&tf_core_framework.TensorShapeProto_Dim{
+							Size: int64(1),
+						},
+					},
+				},
+				StringVal: [][]byte{imageBytes},
+			},
+		},
+	}
+
+	conn, err := grpc.Dial(servingAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Cannot connect to the grpc server: %v\n", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewPredictionServiceClient(conn)
+
+	resp, err := client.Predict(context.Background(), request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tp, ok := resp.Outputs["classes"]
+	if !ok {
+		log.Fatalln(err)
+	}
+	return tp
+}
